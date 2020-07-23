557bd8bdb0666ea79c3bb2814d73e831fd1cef5d
Jaime Fabricio Baño Mera <66963732+fabriciobm43@users.noreply.github.com>
2020-07-23 18:23:18 -0500

    crear clases

---
+++ app/src/main/java/com/example/evaluacionparcial/Adaptadores/Adapter.java
@@ -0,0 +1,5 @@
+package com.example.evaluacionparcial.Adaptadores;
+
+public class Adapter {
+
+}
---
+++ app/src/main/java/com/example/evaluacionparcial/Modelo/Banderas.java
@@ -0,0 +1,8 @@
+package com.example.evaluacionparcial.Modelo;
+
+import android.widget.ImageView;
+
+public class Banderas {
+        String name;
+        ImageView imagen;
+}
