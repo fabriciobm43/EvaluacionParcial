1063f7cf9c45e8817e41671652d6d5f836d8f354
Jaime Fabricio Baño Mera <66963732+fabriciobm43@users.noreply.github.com>
2020-07-23 17:45:47 -0500

    Implementacion de fragment con NavigationView

--- app/src/main/java/com/example/evaluacionparcial/MainActivity.java
+++ app/src/main/java/com/example/evaluacionparcial/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.evaluacionparcial;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
@@ -31,7 +34,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+        boolean fragmentTransaction = false;
+
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.menu_seccion_1:
+                fragment = new Fragment_General();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_2:
+                fragment = new Fragment_Lenguaje();
+                fragmentTransaction = true;
+                break;
+
+        }
+
+        if(fragmentTransaction) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+
+            menuItem.setChecked(true);
+            getSupportActionBar().setTitle(menuItem.getTitle());
+        }
+        drawerLayout.closeDrawers();
+        return true;
     }
 }
