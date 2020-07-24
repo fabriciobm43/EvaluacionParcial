14d71bd312e05945e722548ff549e793c842589c
Jaime Fabricio Baño Mera <66963732+fabriciobm43@users.noreply.github.com>
2020-07-23 22:29:02 -0500

    cargar

--- app/src/main/java/com/example/evaluacionparcial/MainActivity.java
+++ app/src/main/java/com/example/evaluacionparcial/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.evaluacionparcial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,6 +24,7 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.evaluacionparcial.Adaptadores.Adaptador;
 import com.example.evaluacionparcial.Adaptadores.Banderas;
+import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,23 +32,31 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     String url = "https://restcountries.eu/rest/v2/all";
     RecyclerView recyclerView;
     Adaptador adaptor;
     ArrayList<Banderas> banderas;
+    DrawerLayout drawerLayout;
+    NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
         recyclerView = findViewById(R.id.recycler_banderas);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adaptor = new Adaptador();
         recyclerView.setAdapter(adaptor);
         banderas = new ArrayList<>();
         getData();
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icono_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navView = (NavigationView)findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
 
         /*recyclerView = (RecyclerView) findViewById(R.id.recycler_banderas);
         recyclerView.setHasFixedSize(true);
@@ -53,6 +69,11 @@ public class MainActivity extends AppCompatActivity {
                 datos, MainActivity.this, MainActivity.this);
         ws.execute("GET");*/
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar , menu);
+        return true;
     }
     private void getData() {
         final ProgressDialog progressDialog = new ProgressDialog(this);
@@ -90,6 +111,46 @@ public class MainActivity extends AppCompatActivity {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(jsonArrayRequest);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        boolean fragmentTransaction = false;
+
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.menu_seccion_1:
+                fragment = new Fragment_General();
+                fragmentTransaction = true;
+                break;
+            case R.id.menu_seccion_2:
+                fragment = new Fragment_Lenguaje();
+                fragmentTransaction = true;
+                break;
+
+        }
+
+        if(fragmentTransaction) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+
+            menuItem.setChecked(true);
+            getSupportActionBar().setTitle(menuItem.getTitle());
+        }
+        drawerLayout.closeDrawers();
+        return true;
     }
 
 
